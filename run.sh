c66efaba3d7b44603b259b71fbfa8d5e4a139314
Matt Phillips <mattphillipsphd@yahoo.com>
2015-07-07 06:37:44 -0400

    Multi-threaded version

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-rm -f tweet_digest*
+rm -f tweet_digest*;
 
 if [[ $OSTYPE == "linux-gnu" ]]; then
-	g++ -std=c++11 src/main.cpp src/tweet.cpp src/summary.cpp -lpthread -o tweet_digest
+	g++ -std=c++11 src/main.cpp src/tweet.cpp src/runningmedian.cpp src/tweetwords.cpp -lpthread -o tweet_digest
 else
-	g++ -std=c++11 src/main.cpp src/tweet.cpp src/summary.cpp -o tweet_digest
+	g++ -std=c++11 src/main.cpp src/tweet.cpp src/runningmedian.cpp src/tweetwords.cpp -o tweet_digest
 fi
 
 ./tweet_digest
