d49d0dccd9e2a8607477ddbf17f000fc660ea7c4
Matt Phillips <mattphillipsphd@yahoo.com>
2015-07-06 22:00:25 -0400

    Single-threaded version

--- run.sh
+++ run.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# example of the run script for running the word count
+g++ -std=c++11 src/main.cpp src/tweet.cpp src/summary.cpp -o tweet_digest
 
-# I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-python ./src/words_tweeted.py ./tweet_input/tweets.txt ./tweet_output/ft1.txt
-python ./src/median_unique.py ./tweet_input/tweets.txt ./tweet_output/ft2.txt
-
-
-
+./tweet_digest
