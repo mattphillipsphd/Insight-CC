24daad19e8483912292b8daa2695a6681c774c6a
Matt Phillips <mattphillipsphd@yahoo.com>
2015-07-07 04:50:58 -0400

    Eliminated empty words due to initial white space

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-g++ -std=c++11 src/main.cpp src/tweet.cpp src/summary.cpp -o tweet_digest
+rm -f tweet_digest*
+
+if [[ $OSTYPE == "linux-gnu" ]]; then
+	g++ -std=c++11 src/main.cpp src/tweet.cpp src/summary.cpp -lpthread -o tweet_digest
+else
+	g++ -std=c++11 src/main.cpp src/tweet.cpp src/summary.cpp -o tweet_digest
+fi
 
 ./tweet_digest
